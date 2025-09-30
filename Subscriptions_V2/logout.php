32e97da0ade5792ff9b57461d48b1e9a5e81ce67
abhishek12314 <114276599+abhishek12314@users.noreply.github.com>
2025-09-30 21:08:24 +0530

    logout.php

--- Subscriptions_V2/logout.php
+++ Subscriptions_V2/logout.php
@@ -1,13 +1,7 @@
 <?php
 session_start();
-
-// Unset all session variables
 $_SESSION = [];
-
-// Destroy the session
 session_destroy();
-
-// Destroy session cookie too (extra safety)
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -15,8 +9,6 @@ if (ini_get("session.use_cookies")) {
         $params["secure"], $params["httponly"]
     );
 }
-
-// Redirect to login page
 header("Location: login.html");
 exit();
 ?>
